1106d65d0049f53ce3d1dfc9b0993586c1bdc60b
esparano <evansparano@gmail.com>
2026-06-13 11:56:10 -0400

    Fix rule bot test passing unintentionally

--- zoo/board_games/galcon/envs/rule_bot.py
+++ zoo/board_games/galcon/envs/rule_bot.py
@@ -12,7 +12,7 @@ class GalconRandomBot:
     def get_action(self) -> int:
         non_pass_actions = [action for action in self.env.legal_actions if action != self.env.pass_action]
         if len(non_pass_actions) > 0:
-            return int(np.random.choice(self.env.legal_actions))
+            return int(np.random.choice(non_pass_actions))
         return int(self.env.pass_action)
 
 
